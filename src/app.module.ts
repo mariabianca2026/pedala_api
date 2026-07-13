c0bbe5794ee4df8c870d2c83a01ff0fe5474e70e
mariabianca <mariabiancabianca642@gmail.com>
2026-07-13 14:28:23 -0300

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { UsuarioModule } from './usuario/usuario.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { join } from 'path';
 
 @Module({
-  imports: [UsuarioModule],
+  imports: [
+  ConfigModule.forRoot({
+    isGlobal: true,
+    envFilePath: '.env'
+  }),
+  TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: "postgres",
+    password: "102030",
+    database: "pedaladbt",
+    migrations: [
+      join(__dirname,'migrations', '*{.ts,.js'),
+    ],
+    migrationsRun: true
+  }),
+    UsuarioModule],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ src/migrations/1783452266258-CreateUuidExtension.ts
@@ -0,0 +1,18 @@
+import { EXCEPTION_FILTERS_METADATA } from "@nestjs/common/constants";
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateUuidExtension1783452266258 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+       await queryRunner.query(`
+            CREATE EXTENSION IF NOT EXISTS "pgcrypto";     
+        `)
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+       await queryRunner.query(`
+            DROP EXTENSION IF EXISTS "pgcrypto";
+        `)
+    }
+
+}
